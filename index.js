aab40e887993517ba899067abfacc9724a47865e
QuantumMapleQC <thefattestmod@gmail.com>
2025-04-26 20:20:50 -0400

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-// Simple JavaScript to display a message
-document.addEventListener('DOMContentLoaded', function () {
-    alert('Hello!!! you are currently seeing this because this is a Java Popup about the fact that you can navigate this website on older Browsers / Hardwares that supports older browsers IE6/7! have fun :>');
-  });
-  
